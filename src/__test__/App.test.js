8312d4ecf9b6d1c44d7cfc92ec0c56284dd8d4fe
Alhaimouni <m.alhaumouni@gmail.com>
2022-09-05 18:40:20 +0300

    adding test

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -2,30 +2,37 @@ import React from "react";
 import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
 import App from '../App';
+import { wait } from "@testing-library/user-event/dist/utils";
 
 test('Testing the text', async ()=>{
 
     render(<App/>);
     const name = await screen.findByTestId('name');
-    expect(name).toHaveTextContent('My name is meow');
+    expect(name).toHaveTextContent('Your Name');
 
 })
 
 
-// test('Testing the name changing',async ()=>{
 
-//     render(<App/>);
-//     const name = await screen.findByTestId('name');
-//     const inputFeild = await screen.findByTestId('input');
-//     fireEvent.change(inputFeild,{target:{value:'walla'}});
-//     expect(name).toHaveTextContent('My name is walla');
+test('Testing the name changing', async () => {
 
-// })
+    render(<App />);
+    const age = await screen.findByTestId('age');
+    expect(age).toHaveTextContent('Your age after 5 years');
+    // const inputFeild = await screen.findByTestId('ageField');
+    // const submit = await screen.findByTestId('submit');
+    // const form = await screen.findByTestId('form');
+    // fireEvent.submit(inputFeild, { target: { id: {value : 25} } });
+})
 
-// test('Testing the submit', async ()=>{
-//     render(<App/>);
-//     const name = await screen.findByTestId('name');
-//     const submitField = await screen.findByTestId('submit');
-//     fireEvent.submit(submitField,{trager:{value:'Hello'}});
-//     expect(name).toHaveTextContent('My name is Hello');
-// })
+
+
+    // test('Testing the name changing',async ()=>{
+    
+    //     render(<App/>);
+    //     const name = await screen.findByTestId('name');
+    //     const inputFeild = await screen.findByTestId('input');
+    //     fireEvent.change(inputFeild,{target:{value:'walla'}});
+    //     expect(name).toHaveTextContent('My name is walla');
+    
+    // })
--- src/components/Person.js
+++ src/components/Person.js
@@ -6,18 +6,18 @@ function Person(props) {
 
   return (
     <div>
-        <h1>{props.name}</h1>
-        <h4>{props.age}</h4>
+        <h1 data-testid='name'>{props.name}</h1>
+        <h4 data-testid='age'>{props.age}</h4>
         <h4>{props.gender}</h4>
 
-        <form onSubmit={props.func}>
+        <form onSubmit={props.func} data-testid='form'>
             <input type='text' placeHolder='edit name' name='n'/>
             <br></br>
-            <input type='text' placeHolder='edit age' name='a'/>
+            <input type='text' placeHolder='edit age' name='a' data-testid='ageFeild'/>
             <br></br>
             <input type='text' placeHolder='edit gender' name='g'/>
             <br></br>
-            <input type='submit' value='Press to edit'></input>
+            <input type='submit' value='Press to edit' data-testid='submit'></input>
             {/* <label><input type='radio' id='male' name ='gender' placeHolder='edit age' value="Male" checked/> Male</label>
             <label><input type='radio' name ='gender' placeHolder='edit age' value="Female"/> Female</label> */}  
         </form>
